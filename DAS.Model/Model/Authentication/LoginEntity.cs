61cc34c0c1839fc23712223d406ca26e36b32778
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-19 17:34:39 +0300

    left the login props from Userentity and created LoginEntity

---
+++ DAS.Model/Model/Authentication/LoginEntity.cs
@@ -0,0 +1,42 @@
+﻿using DAS.Model.Base;
+using DAS.Model.Base.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Model.Model.Authentication
+{
+    public class LoginEntity:BaseEntity,IBaseTimeEntity
+    {
+        public LoginEntity():base()
+        {
+
+        }
+
+        public LoginEntity(string username,string password):base()
+        {
+            this.Username = username;
+            this.Password = Password;
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get ; set ; }
+        public DateTime? DeletedAt { get; set ;}        
+    }
+
+    public class LoginValidation:AbstractValidator<LoginEntity>
+    {
+        public LoginValidation()
+        {
+            RuleFor(x => x.Username).Length(5, 50);
+            RuleFor(x => x.Password).Length(5, 50);
+            RuleFor(x => x.CreatedAt).NotEmpty().NotNull();
+        }
+    }
+}
--- DAS.Model/Model/User/UserEntity.cs
+++ DAS.Model/Model/User/UserEntity.cs
@@ -1,9 +1,11 @@
 ﻿using DAS.Model.Base;
 using DAS.Model.Base.Interfaces;
+using DAS.Model.Model.Authentication;
 using DAS.Model.Model.Enums;
 using FluentValidation;
 using FluentValidation.Validators;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAS.Model.Model.User
 {
@@ -16,50 +18,23 @@ namespace DAS.Model.Model.User
 
         public UserEntity(string name,string surname,string email,string username,string password,RoleEnum userRole=RoleEnum.User):base()
         {
-            //name control
-            if (String.IsNullOrEmpty(name))
-                this.Name = "Empty";
-            else
                 this.Name = name.ToLower();
-
-            //surname control
-            if (String.IsNullOrEmpty(surname))
-                this.Surname = surname;
-            else
                 this.Surname = surname.ToLower();
-
-            //email
-            if (String.IsNullOrEmpty(email))
-                this.Email = "Empty";
-            else
                 this.Email = email;
-
-            //username
-            if (String.IsNullOrEmpty(username))
-                this.Username = "Empty";
-            else
-                this.Username = username;
-
-            //password
-            if (String.IsNullOrEmpty(password))
-                this.Password = "Empty";
-            else
-                this.Password = password;
-
-            //User Role control
-            if (userRole != RoleEnum.User)
                 this.UserRole = userRole;
         }
 
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
+        public Guid LoginId { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? ModifiedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
         public RoleEnum? UserRole { get; set; } = RoleEnum.User;
+
+        [ForeignKey("LoginId")]
+        public LoginEntity Login { get; set; }
 
     }
 
@@ -70,7 +45,6 @@ namespace DAS.Model.Model.User
             RuleFor(x => x.CreatedAt).NotEmpty().NotNull();
             RuleFor(x => x.Email).EmailAddress(EmailValidationMode.Net4xRegex);
             RuleFor(x => x.Name).Length(5, 50);
-            RuleFor(x => x.Password).MinimumLength(6);
             RuleFor(x => x.UserRole).NotNull().NotEmpty();
         }
     }
