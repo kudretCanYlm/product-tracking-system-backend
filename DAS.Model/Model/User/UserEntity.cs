e280241ce7c5afa08a47e5204c59ed6502438adc
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-19 17:13:50 +0300

    added fluentValidation

--- DAS.Model/Model/User/UserEntity.cs
+++ DAS.Model/Model/User/UserEntity.cs
@@ -1,6 +1,8 @@
 ﻿using DAS.Model.Base;
 using DAS.Model.Base.Interfaces;
 using DAS.Model.Model.Enums;
+using FluentValidation;
+using FluentValidation.Validators;
 using System;
 
 namespace DAS.Model.Model.User
@@ -12,7 +14,7 @@ namespace DAS.Model.Model.User
 
         }
 
-        public UserEntity(string name,string surname,string email,string username,string password,RoleEnum userRole=RoleEnum.User)
+        public UserEntity(string name,string surname,string email,string username,string password,RoleEnum userRole=RoleEnum.User):base()
         {
             //name control
             if (String.IsNullOrEmpty(name))
@@ -59,5 +61,17 @@ namespace DAS.Model.Model.User
         public string Password { get; set; }
         public RoleEnum? UserRole { get; set; } = RoleEnum.User;
 
+    }
+
+    public class UserValidation:AbstractValidator<UserEntity>
+    {
+        public UserValidation()
+        {
+            RuleFor(x => x.CreatedAt).NotEmpty().NotNull();
+            RuleFor(x => x.Email).EmailAddress(EmailValidationMode.Net4xRegex);
+            RuleFor(x => x.Name).Length(5, 50);
+            RuleFor(x => x.Password).MinimumLength(6);
+            RuleFor(x => x.UserRole).NotNull().NotEmpty();
+        }
     }
 }
