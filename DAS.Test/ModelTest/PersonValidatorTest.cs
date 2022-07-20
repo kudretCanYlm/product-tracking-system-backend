004e02513006fa1d63b922a2357d26fccfdb7ea2
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-20 18:40:55 +0300

    first commit

---
+++ DAS.Test/ModelTest/PersonValidatorTest.cs
@@ -0,0 +1,34 @@
+﻿using DAS.Model.Model.Authentication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.TestHelper;
+
+namespace DAS.Test.ModelTest
+{
+    [TestClass]
+    public class PersonValidatorTest
+    {
+
+        LoginValidation validator = new LoginValidation();
+
+        [TestMethod]
+        public void Should_have_error_when_Username_and_Password_and_created_is_null()
+        {
+            //arrange
+            LoginEntity loginEntity = new LoginEntity();
+            loginEntity.CreatedAt = null;
+
+            //act
+            var result= validator.TestValidate(loginEntity);
+
+            //assert
+            result.ShouldHaveAnyValidationError();
+
+        }
+    }
+}
---
+++ DAS.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DAS.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DAS.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0ce71a0-21a8-4168-a416-c42283911fce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
