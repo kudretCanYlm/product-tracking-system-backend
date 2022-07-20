a22932c6f0864ca1450f0164a6073309588c5ab8
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-20 18:05:59 +0300

    first commit

---
+++ DAS.Model/Model/Media/ImageEntity.cs
@@ -0,0 +1,36 @@
+﻿using DAS.Model.Base;
+using DAS.Model.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace DAS.Model.Model.Media
+{
+    public class ImageEntity : BaseEntity, IBaseTimeEntity
+    {
+        public ImageEntity():base()
+        {
+
+        }
+        
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get ; set ; }
+        public DateTime? DeletedAt { get ;set ;}
+    }
+
+    public class ImageValidation : AbstractValidator<ImageEntity>
+    {
+        public ImageValidation()
+        {
+            RuleFor(x => x.CreatedAt).NotNull().NotEmpty();
+            RuleFor(x => x.ImageData).NotNull().NotEmpty();
+            RuleFor(x => x.ImageMimeType).Length(2, 50);
+        }
+    }
+
+}
