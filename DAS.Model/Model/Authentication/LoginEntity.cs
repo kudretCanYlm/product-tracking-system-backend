782770ebde4c96f4bde4fde682dec4d0a70b2654
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-26 19:01:13 +0300

    added CreateMD5 Method

--- DAS.Model/Model/Authentication/LoginEntity.cs
+++ DAS.Model/Model/Authentication/LoginEntity.cs
@@ -1,10 +1,12 @@
 ﻿using DAS.Model.Base;
+using DAS.Model.Base.Enums;
 using DAS.Model.Base.Interfaces;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,14 +22,41 @@ namespace DAS.Model.Model.Authentication
         public LoginEntity(string username,string password):base()
         {
             this.Username = username;
-            this.Password = Password;
+            this.Password = password;
         }
 
         public string Username { get; set; }
         public string Password { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? ModifiedAt { get ; set ; }
-        public DateTime? DeletedAt { get; set ;}        
+        public DateTime? DeletedAt { get; set ;}
+
+        //we will add recursive function
+        public void CreateMD5ForUsernameAndPassword()
+        {
+            // Use input string to calculate MD5 hash
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytesUsername = Encoding.ASCII.GetBytes(this.Username);
+                byte[] inputBytesPassword = Encoding.ASCII.GetBytes(this.Password);
+
+                byte[] hashBytesUsername = md5.ComputeHash(inputBytesUsername);
+                byte[] hashBytesPassword = md5.ComputeHash(inputBytesPassword);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sbUserName = new StringBuilder();
+                StringBuilder sbPassword = new StringBuilder();
+
+                for (int i = 0; i < hashBytesUsername.Length; i++)
+                    sbUserName.Append(hashBytesUsername[i].ToString("X2"));
+
+                for (int i = 0; i < hashBytesPassword.Length; i++)
+                    sbPassword.Append(hashBytesPassword[i].ToString("X2"));
+
+                this.Username = sbUserName.ToString();
+                this.Password = sbPassword.ToString();
+            }
+        }
     }
 
     public class LoginValidation:AbstractValidator<LoginEntity>
