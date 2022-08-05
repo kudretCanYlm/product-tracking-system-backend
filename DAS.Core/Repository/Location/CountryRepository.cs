5888709c6acdaf4e20c6efc59dc4e81ee8a2f2a1
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-05 16:51:20 +0300

    first commit

---
+++ DAS.Core/Repository/Location/CountryRepository.cs
@@ -0,0 +1,23 @@
+﻿using DAS.Core.Infrastructure;
+using DAS.Model.Model.Location;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Core.Repository.Location
+{
+    public class CountryRepository:RepositoryBase<CountryEntity>,ICountryRepository
+    {
+        public CountryRepository(IDatabaseFactory databaseFactory):base(databaseFactory)
+        {
+
+        }
+    }
+
+    public interface ICountryRepository:IRepository<CountryEntity>
+    {
+
+    }
+}
