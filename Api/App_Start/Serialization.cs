c09a8b4c706333bdcb23c707d15fa1f2a93ebe89
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-05 16:56:33 +0300

    Revert "Revert "refactor""
    This reverts commit 43866d32b2ef00fb112002ae193b2a61763852b3.

---
+++ Api/App_Start/Serialization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Results;
+
+namespace Api.App_Start
+{
+    public class Serialization
+    {
+        
+    }
+}
---
+++ Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using Api.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //web api close xml formats and enable json formats
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+            config.Formatters.Add(new BrowserJsonFormatter());
+        }
+    }
+}
