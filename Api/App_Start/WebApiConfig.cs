51177f016584af63e9709f05d70c69b901e0933e
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-06 20:33:29 +0300

    changed route

--- Api/App_Start/WebApiConfig.cs
+++ Api/App_Start/WebApiConfig.cs
@@ -18,7 +18,7 @@ namespace Api
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
