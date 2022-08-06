351ac215690c700d83ad5cb31b380aa3951173e7
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-06 20:34:07 +0300

    added new country controller

---
+++ Api/Controllers/CountryController.cs
@@ -0,0 +1,67 @@
+﻿using Api.Infrastructure.Attributes;
+using DAS.Model.Model.Location;
+using DAS.Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class CountryController : ApiController
+    {
+        ICountyService countyService;
+
+        public CountryController(ICountyService countyService)
+        {
+            this.countyService = countyService;
+        }
+
+        [CacheControl]
+        public HttpResponseMessage Get()
+        {
+            var values = countyService.GetCountries();
+
+            return Request.CreateResponse(HttpStatusCode.OK, values);
+        }
+        [CacheControl]
+        public HttpResponseMessage Get([FromUri] Guid id)
+        {
+            var values = countyService.GetCountriesWithFilter(x => x.Id == id);
+
+            return Request.CreateResponse(HttpStatusCode.OK, values);
+        }
+
+        public HttpResponseMessage Post([FromBody] CountryEntity value)
+        {
+            string message = countyService.AddCountry(value);
+
+            return Request.CreateResponse(HttpStatusCode.OK, message);
+        }
+
+        public HttpResponseMessage Put([FromUri] Guid id, [FromBody] CountryEntity countryUpdate)
+        {
+            var country = countyService.GetSingleCountry(id);
+
+            if (country != null )
+            {
+                if(countryUpdate!=null)
+                {
+                    country.CountryName = countryUpdate.CountryName;
+                    country.CountryCode = countryUpdate.CountryCode;
+
+                    string message = countyService.UpdateCountry(country);
+
+                    return Request.CreateResponse(HttpStatusCode.OK, message);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "null counrty");
+                
+            }
+
+            return Request.CreateResponse(HttpStatusCode.BadRequest, "not find country");
+        }
+    }
+}
