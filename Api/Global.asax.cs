eed68c33d2d004d93c05f9e870f7da78f6793a98
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-13 18:02:53 +0300

    remove mvc options

--- Api/Global.asax.cs
+++ Api/Global.asax.cs
@@ -14,10 +14,7 @@ namespace Api
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            //RouteConfig.RegisterRoutes(RouteTable.Routes);;
             Bootstrapper.Run();
         }
     }
