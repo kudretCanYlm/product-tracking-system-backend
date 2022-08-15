ef6c4ecf9d8f6d2ad054540a9393e616709e42e7
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-15 19:31:31 +0300

    remove unused namespaces

--- DAS.Core/Infrastructure/DatabaseFactory.cs
+++ DAS.Core/Infrastructure/DatabaseFactory.cs
@@ -1,9 +1,4 @@
 ﻿using DAS.Data.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAS.Core.Infrastructure
 {
