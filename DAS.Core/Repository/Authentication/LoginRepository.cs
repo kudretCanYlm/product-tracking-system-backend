4425bfc57371bbb39e0ea88247123a55140f5691
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-17 19:56:09 +0300

    added new functions

--- DAS.Core/Repository/Authentication/LoginRepository.cs
+++ DAS.Core/Repository/Authentication/LoginRepository.cs
@@ -19,11 +19,24 @@ namespace DAS.Core.Repository.Authentication
         {
             return Get(x => x.Username == username) != null;
         }
+        public LoginEntity GetUser(string username, string password)
+        {
+            LoginEntity login = new LoginEntity();
+
+            login.Name = null;
+            login.Surname = null;
+            login.Username = username;
+            login.Password = password;
+            login.CreateMD5ForUsernameAndPassword();
+
+            return Get(x => x.Username == login.Username && x.Password == login.Password) ?? login;
+        }
     }
 
     public interface ILoginRepository : IRepository<LoginEntity>
     {
         bool IsMailUsing(string email);
         bool IsUsernameUsing(string username);
+        LoginEntity GetUser(string username, string password);
     }
 }
