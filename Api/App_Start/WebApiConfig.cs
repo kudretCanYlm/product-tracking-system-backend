c7bc2344ce8afc97b8ae807caf07f88dd0032467
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-23 20:25:33 +0300

    added cors

--- Api/App_Start/WebApiConfig.cs
+++ Api/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Api
 {
@@ -29,6 +30,10 @@ namespace Api
 
             //Jwt
             config.Filters.Add(new AuthorizeAttribute());
+
+            //cors 
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
