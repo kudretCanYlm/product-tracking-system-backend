4f1193573cb92929a9b731f0cfdabed63d656125
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-23 20:26:51 +0300

    first commit

---
+++ Api/Mappings/EntityToView.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Mappings
+{
+    public class EntityToView:Profile
+    {
+        public override string ProfileName => "EntityToViewMap";
+
+        protected override void Configure()
+        {
+            //https://github.com/MarlabsInc/SocialGoal/blob/master/source/SocialGoal/Mappings/DomainToViewModelMappingProfile.cs
+            // Mapper.CreateMap<>
+            base.Configure();
+        }
+    }
+}
