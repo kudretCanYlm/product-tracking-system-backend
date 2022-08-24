5c1cfa58457c5995ad46e53f6479e27ca60c2c41
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-24 20:35:56 +0300

    added new props and their default values

--- DAS.Model/Model/Media/ImageEntity.cs
+++ DAS.Model/Model/Media/ImageEntity.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
+using DAS.Model.Base.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using DAS.Model.Model.Authentication;
 
 namespace DAS.Model.Model.Media
 {
@@ -13,14 +16,19 @@ namespace DAS.Model.Model.Media
     {
         public ImageEntity():base()
         {
-
+            this.ImageType = ImageTypesEnum.UserIcon;
+            this.IsPublic = false;
         }
         
         public byte[] ImageData { get; set; }
         public string ImageMimeType { get; set; }
+        public Guid OwnerId { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? ModifiedAt { get ; set ; }
         public DateTime? DeletedAt { get ;set ;}
+        public ImageTypesEnum ImageType { get; set; }
+        public bool IsPublic { get; set; }
+        
     }
 
     public class ImageValidation : AbstractValidator<ImageEntity>
@@ -30,6 +38,8 @@ namespace DAS.Model.Model.Media
             RuleFor(x => x.CreatedAt).NotNull().NotEmpty();
             RuleFor(x => x.ImageData).NotNull().NotEmpty();
             RuleFor(x => x.ImageMimeType).Length(2, 50);
+            RuleFor(x => x.IsPublic).NotNull().NotEmpty();
+            RuleFor(x => x.OwnerId).NotEmpty().NotNull();
         }
     }
 
