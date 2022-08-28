469cbaa0eb9c1a327f40db45988cb12c32a36d63
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-28 17:06:06 +0300

    changed method attributes

--- Api/Controllers/MediaController.cs
+++ Api/Controllers/MediaController.cs
@@ -73,7 +73,7 @@ namespace Api.Controllers
             return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "not working");
         }
 
-        [Route("getSingleGlobalImage"), HttpGet]
+        [Route("getSingleGlobalImage"), HttpPost,JwtAuthentication(RoleEnum.Admin)]
         public HttpResponseMessage GetSingle([FromBody] ImageGetView imageGetView)
         {
             var imageDb = imageService.GetSingleGlobalImage(imageGetView.OwnerId, imageGetView.ImageType);
