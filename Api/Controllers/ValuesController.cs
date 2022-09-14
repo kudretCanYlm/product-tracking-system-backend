2c342bf4dba6989d653bbaf494be306b68b9df17
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-14 21:14:54 +0300

    apllied ICountyService refactoring

--- Api/Controllers/ValuesController.cs
+++ Api/Controllers/ValuesController.cs
@@ -1,6 +1,6 @@
 ﻿using Api.Infrastructure.Attributes;
 using DAS.Model.Model.Location;
-using DAS.Service.Services;
+using DAS.Service.Services.Location;
 using System;
 using System.Collections.Generic;
 using System.Linq;
