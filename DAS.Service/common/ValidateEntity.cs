73169fbe75261a7a260e2a232562da91bfdc3b16
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-14 20:48:26 +0300

    extensions changed as IValidator<T>

--- DAS.Service/common/ValidateEntity.cs
+++ DAS.Service/common/ValidateEntity.cs
@@ -7,12 +7,12 @@ namespace DAS.Service.common
 {
     public static class ValidateEntity
     {
-        public static bool IsValidEntity<T>(this AbstractValidator<T> validator, T entity) where T : BaseEntity
+        public static bool IsValidEntity<T>(this IValidator<T> validator, T entity) where T : BaseEntity
         {
             return ValidateResultEntity(validator, entity).IsValid;
         }
 
-        public static string GetValidErrorMessages<T>(this AbstractValidator<T> validator, T entity) where T : BaseEntity
+        public static string GetValidErrorMessages<T>(this IValidator<T> validator, T entity) where T : BaseEntity
         {
             StringBuilder errors = new StringBuilder();
 
@@ -22,7 +22,7 @@ namespace DAS.Service.common
             return errors.ToString();
         }
 
-        private static ValidationResult ValidateResultEntity<T>(AbstractValidator<T> validator, T entity) where T : BaseEntity
+        private static ValidationResult ValidateResultEntity<T>(IValidator<T> validator, T entity) where T : BaseEntity
         {
             return validator.Validate(entity);
         }
