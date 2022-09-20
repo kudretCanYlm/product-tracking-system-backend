9c98c204a53f7d0f4bdcb0790a8893553b067bfe
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-20 13:49:37 +0300

    added SetMessagesIsViewedTrue method

--- DAS.Core/Repository/Chat/MessageRepository.cs
+++ DAS.Core/Repository/Chat/MessageRepository.cs
@@ -3,21 +3,31 @@ using DAS.Model.Model.Chat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAS.Core.Repository.Chat
 {
-    public class MessageRepository:RepositoryBase<MessageEntity>,IMessageRepository
+    public class MessageRepository : RepositoryBase<MessageEntity>, IMessageRepository
     {
-        public MessageRepository(IDatabaseFactory databaseFactory):base(databaseFactory)
+        public MessageRepository(IDatabaseFactory databaseFactory) : base(databaseFactory)
         {
 
         }
+
+        public void SetMessagesIsViewedTrue(IEnumerable<MessageEntity> messages)
+        {
+            foreach (var message in messages)
+                message.IsViewed = true;
+
+            UpdateMany(messages);
+            
+        }
     }
 
-    public interface IMessageRepository:IRepository<MessageEntity>
+    public interface IMessageRepository : IRepository<MessageEntity>
     {
-
+        void SetMessagesIsViewedTrue(IEnumerable<MessageEntity> messages);
     }
 }
