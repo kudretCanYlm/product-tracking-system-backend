bf6bed07186e3cc45a20f56042506c7aa1c0b7c8
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-20 13:49:12 +0300

    added IsUnique method

--- DAS.Core/Repository/Chat/ChatRepository.cs
+++ DAS.Core/Repository/Chat/ChatRepository.cs
@@ -14,10 +14,19 @@ namespace DAS.Core.Repository.Chat
         {
 
         }
+
+        public bool IsUnique(ChatEntity chatEntity)
+        {
+          return  GetMany(x =>
+            (x.UserFirstId == chatEntity.UserFirstId && x.UserSecondId == chatEntity.UserSecondId)
+            ||
+            (x.UserFirstId == chatEntity.UserSecondId && x.UserSecondId == chatEntity.UserFirstId)
+            ).Count() == 0;
+        }
     }
 
     public interface IChatRepository:IRepository<ChatEntity>
     {
-
+        bool IsUnique(ChatEntity chatEntity);
     }
 }
