2ac715b6ced15cf9d8082b52ac858bb3c3246827
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-20 13:51:26 +0300

    added AddChat,FindChat,SaveChat methods

--- DAS.Service/Services/Chat/ChatService.cs
+++ DAS.Service/Services/Chat/ChatService.cs
@@ -1,6 +1,8 @@
 ﻿using DAS.Core.Infrastructure;
 using DAS.Core.Repository.Chat;
 using DAS.Model.Model.Authentication;
+using DAS.Model.Model.Chat;
+using DAS.Service.common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,19 +13,65 @@ namespace DAS.Service.Services.Chat
 {
     public interface IChatService
     {
-
+        object AddChat(ref ChatEntity chatEntity);
+        ChatEntity FindChat(Guid UserFirstId, Guid UserSecondId);
     }
-    public class ChatService:IChatService
+    public class ChatService : IChatService
     {
         private readonly IChatRepository chatRepository;
         private readonly IUnitOfWork unitOfWork;
-        private readonly ILoginValidation validator;
+        private readonly IChatValidation validator;
 
-        public ChatService(IChatRepository chatRepository, IUnitOfWork unitOfWork, ILoginValidation validator)
+        public ChatService(IChatRepository chatRepository, IUnitOfWork unitOfWork, IChatValidation validator)
         {
             this.chatRepository = chatRepository;
             this.unitOfWork = unitOfWork;
             this.validator = validator;
+        }
+
+        public object AddChat(ref ChatEntity chatEntity)
+        {
+            chatEntity.CreatedAt = DateTime.Now;
+
+            if (validator.IsValidEntity(chatEntity))
+            {
+                if (chatRepository.IsUnique(chatEntity))
+                {
+                    chatRepository.Add(chatEntity);
+                    
+                    return SaveChat();
+                }
+
+                return "this chat already exists";
+            }
+
+            return validator.GetValidErrorMessages(chatEntity);
+
+        }
+
+        public ChatEntity FindChat(Guid UserFirstId, Guid UserSecondId)
+        {
+            return chatRepository
+                .Get(
+                    x =>
+                        (x.UserFirstId == UserFirstId && x.UserSecondId == UserSecondId)
+                        ||
+                        (x.UserFirstId == UserSecondId && x.UserSecondId == UserFirstId))
+                ?? null;
+        }
+
+        private object SaveChat()
+        {
+            try
+            {
+                unitOfWork.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
         }
     }
 }
