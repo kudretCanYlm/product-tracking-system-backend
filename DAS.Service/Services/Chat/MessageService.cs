6df91f6895cdd1f0e3671a9423e8c75d01c28b17
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-20 13:52:18 +0300

    added AddMessage,GetMessages ,SaveMessageWithMessage,SaveMessage methods

--- DAS.Service/Services/Chat/MessageService.cs
+++ DAS.Service/Services/Chat/MessageService.cs
@@ -1,9 +1,11 @@
 ﻿using DAS.Core.Infrastructure;
 using DAS.Core.Repository.Chat;
 using DAS.Model.Model.Chat;
+using DAS.Service.common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +13,8 @@ namespace DAS.Service.Services.Chat
 {
     public interface IMessageService
     {
-
+        object AddMessage(MessageEntity messageEntity);
+        IEnumerable<MessageEntity> GetMessages(Expression<Func<MessageEntity, bool>> where);
     }
 
     public class MessageService : IMessageService
@@ -25,6 +28,53 @@ namespace DAS.Service.Services.Chat
             this.messageRepository = messageRepository;
             this.unitOfWork = unitOfWork;
             this.validator = validator;
+        }
+
+        public object AddMessage(MessageEntity messageEntity)
+        {
+            messageEntity.CreatedAt = DateTime.Now;
+
+            if (validator.IsValidEntity(messageEntity))
+            {
+                messageRepository.Add(messageEntity);
+                return SaveMessageWithMessage();
+            }
+
+            return validator.GetValidErrorMessages(messageEntity);
+        }
+
+        public IEnumerable<MessageEntity> GetMessages(Expression<Func<MessageEntity, bool>> where)
+        {
+            var messages=messageRepository.GetMany(where).OrderBy(x => x.CreatedAt).ToList() ?? null;
+            var isNotViewMessages = messageRepository.GetMany(where).Where(x=>x.IsViewed==false) ?? null;
+
+            if (isNotViewMessages != null)
+            {
+                messageRepository.SetMessagesIsViewedTrue(isNotViewMessages);
+                SaveMessage();
+            }
+                
+            return messages;
+
+        }
+
+        private object SaveMessageWithMessage()
+        {
+            try
+            {
+                unitOfWork.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+        }
+
+        private void SaveMessage()
+        {
+            unitOfWork.Commit();
         }
     }
 }
