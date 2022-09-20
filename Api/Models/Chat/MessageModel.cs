4d641e2eb68cd0b0145b6564f1dece09e70ea4a9
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-20 14:43:00 +0300

    first commit

---
+++ Api/Models/Chat/MessageModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models.Chat
+{
+    public class MessageViewModel
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ChatId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+
+    public class GetMessagePostModel
+    {
+        public string TargetUserId { get; set; }
+    }
+
+    public class SendMessagePostModel
+    {
+        public string TargetUserId { get; set; }
+        public string message { get; set; }
+
+    }
+}
