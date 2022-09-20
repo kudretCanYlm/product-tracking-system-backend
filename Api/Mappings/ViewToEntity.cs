5a4697177d83ddebe433aff644114631c9ad84b0
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-20 14:41:51 +0300

    first commit

---
+++ Api/Mappings/ViewToEntity.cs
@@ -0,0 +1,20 @@
+﻿using Api.Models.Chat;
+using AutoMapper;
+using DAS.Model.Model.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Mappings
+{
+    public class ViewToEntity:Profile
+    {
+        public override string ProfileName => "ViewToEntityMap";
+
+        protected override void Configure()
+        {
+            
+        }
+    }
+}
