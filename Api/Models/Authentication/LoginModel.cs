df08632f8dffb09565a2bcab43d3c4dab41428c1
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-22 19:40:31 +0300

    first commit

---
+++ Api/Models/Authentication/LoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models.Authentication
+{
+    public class LoginPostModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginPostModelNewUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Api/Models/Location/CountryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models.Location
+{
+    public class CountryViewModel
+    {
+        public Guid Id { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+    }
+
+    public class CountryPostModel
+    {
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
