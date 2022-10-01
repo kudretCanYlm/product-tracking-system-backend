871ad23098a419d6ada52dd0cadb1f911585cc76
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-01 15:28:44 +0300

    first commit

---
+++ Api/Controllers/ArticleController.cs
@@ -0,0 +1,39 @@
+﻿using Api.Infrastructure.Attributes;
+using DAS.Service.Services.Article;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    [RoutePrefix("article")]
+    public class ArticleController : ApiController
+    {
+        IArticleService articleService;
+        IArticleCommentService articleCommentService;
+        IArticleLikeDislikeService articleLikeDislikeService;
+        IArticleCommentReplyService articleCommentReplyService;
+
+        public ArticleController(IArticleService articleService, IArticleCommentService articleCommentService, IArticleLikeDislikeService articleLikeDislikeService, IArticleCommentReplyService articleCommentReplyService)
+        {
+            this.articleService = articleService;
+            this.articleCommentService = articleCommentService;
+            this.articleLikeDislikeService = articleLikeDislikeService;
+            this.articleCommentReplyService = articleCommentReplyService;
+        }
+
+        [Route("{id}"), HttpGet, CacheControl, AllowAnonymous]
+        public HttpResponseMessage Get([FromUri] Guid id)
+        {
+            var article = articleService.GetArticleById(id);
+
+            if (article == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "didn't find article");
+            //will add image controller
+            return Request.CreateResponse(HttpStatusCode.Created, "found");
+        }
+    }
+}
