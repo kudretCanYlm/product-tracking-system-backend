4418965d70fc9c0b6ba90b6186432b38f402f750
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-05 18:51:43 +0300

    first commit

---
+++ DAS.Model/Model/Post/PostEntity.cs
@@ -0,0 +1,47 @@
+﻿using DAS.Model.Base;
+using DAS.Model.Base.Interfaces;
+using DAS.Model.Model.Authentication;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Model.Model.Post
+{
+    public class PostEntity : BaseEntity, IBaseTimeEntity
+    {
+        public PostEntity():base()
+        {
+
+        }
+        public Guid PostOwnerId { get; set; }
+        public string PostText { get; set; }
+        public DateTime? CreatedAt { get ; set ; }
+        public DateTime? ModifiedAt { get ; set ; }
+        public DateTime? DeletedAt { get ; set ; }
+
+        [ForeignKey("PostOwnerId")]
+        public LoginEntity PostOwner { get; set; }
+    }
+
+    public interface IPostValidation: IValidator<PostEntity>
+    {
+
+    }
+
+    public class PostValidation:AbstractValidator<PostEntity>,IPostValidation
+    {
+        public PostValidation()
+        {
+            RuleFor(x=>x.PostText)
+                .Length(5).WithMessage("text length greater than 5")
+                .NotNull().WithMessage("don't be null")
+                .NotEmpty().WithMessage("don't be empty");
+
+            RuleFor(x => x.PostOwner).SetValidator(new LoginValidation());
+        }
+    }
+}
---
+++ DAS.Service/Services/Post/PostService.cs
@@ -0,0 +1,60 @@
+﻿using DAS.Core.Infrastructure;
+using DAS.Core.Repository.Post;
+using DAS.Model.Model.Post;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Service.Services.Post
+{
+    public interface IPostService
+    {
+
+    }
+
+    public class PostService:IPostService
+    {
+        private IPostRepository postRepository;
+        private IUnitOfWork unitOfWork;
+        private IPostValidation validator;
+
+        public PostService(IPostRepository postRepository, IUnitOfWork unitOfWork, IPostValidation validator)
+        {
+            this.postRepository = postRepository;
+            this.unitOfWork = unitOfWork;
+            this.validator = validator;
+        }
+
+
+        private object SavePostWithMessage()
+        {
+            try
+            {
+                unitOfWork.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+        }
+
+        private void SavePost()
+        {
+            unitOfWork.Commit();
+        }
+
+        private void BeginTransaction()
+        {
+            unitOfWork.BeginTransaction();
+        }
+        private void RollBack()
+        {
+            unitOfWork.RollBack();
+        }
+
+    }
+}
