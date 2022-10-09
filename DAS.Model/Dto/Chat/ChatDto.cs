393d1bd3adad0fc16b10c4bd7a5312a1eb83f9f9
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-09 19:09:45 +0300

    first commit

---
+++ DAS.Model/Dto/Chat/ChatDto.cs
@@ -0,0 +1,19 @@
+﻿using DAS.Model.Base;
+using DAS.Model.Model.Chat;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Model.Dto.Chat
+{
+    [NotMapped]
+    public class ChatDtoView:ChatEntity
+    {
+        //will add others
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
