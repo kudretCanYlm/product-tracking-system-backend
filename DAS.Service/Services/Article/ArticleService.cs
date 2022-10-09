c171bccc3238d5a1b5d2386e433e1ff993746d2e
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-09 19:04:21 +0300

    added new functions and refactored

--- DAS.Service/Services/Article/ArticleService.cs
+++ DAS.Service/Services/Article/ArticleService.cs
@@ -1,6 +1,9 @@
 ﻿using DAS.Core.Infrastructure;
 using DAS.Core.Repository.Article;
+using DAS.Core.Repository.Authentication;
+using DAS.Model.Dto.Article;
 using DAS.Model.Model.Article;
+using DAS.Service.common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,24 +14,86 @@ namespace DAS.Service.Services.Article
 {
     public interface IArticleService
     {
-        ArticleEntity GetArticleById(Guid articleId);
+        ArticleDtoView GetArticleById(Guid articleId);
+        IEnumerable<ArticleDtoView> GetAllArticles();
+        object AddNewArticle(ref ArticleEntity article);
     }
-    public class ArticleService:IArticleService
+    public class ArticleService : IArticleService
     {
         private readonly IArticleRepository articleRepository;
+        private readonly ILoginRepository loginRepository;
         private readonly IUnitOfWork unitOfWork;
         private readonly IArticleValidation validator;
 
-        public ArticleService(IArticleRepository articleRepository, IUnitOfWork unitOfWork, IArticleValidation validator)
+        public ArticleService(IArticleRepository articleRepository, ILoginRepository loginRepository, IUnitOfWork unitOfWork, IArticleValidation validator)
         {
             this.articleRepository = articleRepository;
+            this.loginRepository = loginRepository;
             this.unitOfWork = unitOfWork;
             this.validator = validator;
         }
 
-        public ArticleEntity GetArticleById(Guid articleId)
+        public object AddNewArticle(ref ArticleEntity article)
         {
-            return articleRepository.Get(x => x.Id == articleId && x.IsPublic==true) ?? null;
+            article.CreatedAt = DateTime.Now;
+
+            if (validator.IsValidEntity(article))
+            {
+                articleRepository.Add(article);
+
+                return SaveArticleWithMessage();
+            }
+
+            return validator.GetValidErrorMessages(article);
+
         }
+
+        public ArticleDtoView GetArticleById(Guid articleId)
+        {
+            var article = articleRepository.GetArticleWithDto(x => x.Id == articleId && x.IsPublic);
+
+            return article;
+        }
+        public IEnumerable<ArticleDtoView> GetAllArticles()
+        {
+            var articles = articleRepository.GetArticlesWithDto(null);
+            return articles;
+        }
+
+        private object SaveArticleWithMessage()
+        {
+            try
+            {
+                unitOfWork.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+        }
+
+        private dynamic TransactionArticle()
+        {
+            try
+            {
+                unitOfWork.BeginTransaction();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                unitOfWork.RollBack();
+
+                return ex.Message;
+            }
+        }
+
+        private void SaveArticle()
+        {
+            unitOfWork.Commit();
+        }
+
     }
 }
