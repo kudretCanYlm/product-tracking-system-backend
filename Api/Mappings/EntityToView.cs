e969efe78eaa40a57c01f7e5382bbd9c383ba02e
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-09 19:07:04 +0300

    added new maps

--- Api/Mappings/EntityToView.cs
+++ Api/Mappings/EntityToView.cs
@@ -1,6 +1,9 @@
-﻿using Api.Models.Chat;
+﻿using Api.Models.Article;
+using Api.Models.Chat;
 using Api.Models.Location;
 using AutoMapper;
+using DAS.Model.Dto.Article;
+using DAS.Model.Model.Article;
 using DAS.Model.Model.Chat;
 using DAS.Model.Model.Location;
 using System;
@@ -10,7 +13,7 @@ using System.Web;
 
 namespace Api.Mappings
 {
-    public class EntityToView:Profile
+    public class EntityToView : Profile
     {
         public override string ProfileName => "EntityToViewMap";
 
@@ -19,7 +22,7 @@ namespace Api.Mappings
             Mapper.CreateMap<MessageEntity, MessageViewModel>()
                 .ForMember(x => x.Id, act => act.MapFrom(x => x.Id))
                 .ForMember(x => x.Message, act => act.MapFrom(x => x.Message))
-                .ForMember(x=>x.Name,act=>act.MapFrom(x=>x.User.Name+" "+x.User.Surname))
+                .ForMember(x => x.Name, act => act.MapFrom(x => x.User.Name + " " + x.User.Surname))
                 .ForMember(x => x.UserId, act => act.MapFrom(x => x.UserId))
                 .ForMember(x => x.CreatedAt, act => act.MapFrom(x => x.CreatedAt));
 
@@ -27,6 +30,26 @@ namespace Api.Mappings
                 .ForMember(x => x.Id, act => act.MapFrom(x => x.Id))
                 .ForMember(x => x.CountryName, act => act.MapFrom(x => x.CountryName))
                 .ForMember(x => x.CountryCode, act => act.MapFrom(x => x.CountryCode));
+
+            Mapper.CreateMap<ArticleEntity, ArticleViewModel>()
+                .ForMember(x => x.Id, act => act.MapFrom(x => x.Id))
+                .ForMember(x => x.ArticleTitle, act => act.MapFrom(x => x.ArticleTitle))
+                .ForMember(x => x.Summary, act => act.MapFrom(x => x.Summary))
+                .ForMember(x => x.Content, act => act.MapFrom(x => x.ArticleContent))
+                .ForMember(x => x.CreatedAt, act => act.MapFrom(x => x.CreatedAt))
+                .ForMember(x => x.ArticleOwnerId, act => act.MapFrom(x => x.ArticleOwner.Id))
+                .ForMember(x => x.OwnerName, act => act.MapFrom(x => x.ArticleOwner.Name))
+                .ForMember(x => x.OwnerSurname, act => act.MapFrom(x => x.ArticleOwner.Surname));
+
+            Mapper.CreateMap<ArticleDtoView, ArticleViewModel>()
+                .ForMember(x => x.Id, act => act.MapFrom(x => x.Id))
+                .ForMember(x => x.ArticleTitle, act => act.MapFrom(x => x.ArticleTitle))
+                .ForMember(x => x.Summary, act => act.MapFrom(x => x.Summary))
+                .ForMember(x => x.Content, act => act.MapFrom(x => x.ArticleContent))
+                .ForMember(x => x.CreatedAt, act => act.MapFrom(x => x.CreatedAt))
+                .ForMember(x => x.ArticleOwnerId, act => act.MapFrom(x => x.OwnerId))
+                .ForMember(x => x.OwnerName, act => act.MapFrom(x => x.OwnerName))
+                .ForMember(x => x.OwnerSurname, act => act.MapFrom(x => x.OwnerSurname));
         }
     }
 }
--- Api/Mappings/ViewToEntity.cs
+++ Api/Mappings/ViewToEntity.cs
@@ -1,7 +1,9 @@
-﻿using Api.Models.Authentication;
+﻿using Api.Models.Article;
+using Api.Models.Authentication;
 using Api.Models.Chat;
 using Api.Models.Location;
 using AutoMapper;
+using DAS.Model.Model.Article;
 using DAS.Model.Model.Authentication;
 using DAS.Model.Model.Chat;
 using DAS.Model.Model.Location;
@@ -27,7 +29,14 @@ namespace Api.Mappings
                 .ForMember(x => x.Password, act => act.MapFrom(x => x.Password))
                 .ForMember(x => x.Email, act => act.MapFrom(x => x.Email))
                 .ForMember(x => x.Name, act => act.MapFrom(x => x.Name))
-                .ForMember(x => x.Surname, act => act.MapFrom(x => x.Surname));                
+                .ForMember(x => x.Surname, act => act.MapFrom(x => x.Surname));
+
+            Mapper.CreateMap<ArticlePostModel, ArticleEntity>()
+                .ForMember(x => x.ArticleTitle, act => act.MapFrom(x => x.ArticleTitle))
+                .ForMember(x => x.Summary, act => act.MapFrom(x => x.Summary))
+                .ForMember(x => x.ArticleContent, act => act.MapFrom(x => x.Content))
+                .ForMember(x => x.IsPublic, act => act.MapFrom(x => x.IsPublic));
+
 
         }
     }
