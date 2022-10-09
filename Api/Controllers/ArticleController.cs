ca0363a8d37a36f437a9ebe8b05ee4c908e9444d
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-09 19:08:42 +0300

    added new apis

--- Api/Controllers/ArticleController.cs
+++ Api/Controllers/ArticleController.cs
@@ -1,5 +1,12 @@
 ﻿using Api.Infrastructure.Attributes;
+using Api.Models.Article;
+using AutoMapper;
+using DAS.Model.Base.Enums;
+using DAS.Model.Model.Article;
+using DAS.Model.Model.Enums;
 using DAS.Service.Services.Article;
+using DAS.Service.Services.Authentication;
+using DAS.Service.Services.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +20,15 @@ namespace Api.Controllers
     public class ArticleController : ApiController
     {
         IArticleService articleService;
+        ILoginService loginService;
         IArticleCommentService articleCommentService;
         IArticleLikeDislikeService articleLikeDislikeService;
         IArticleCommentReplyService articleCommentReplyService;
 
-        public ArticleController(IArticleService articleService, IArticleCommentService articleCommentService, IArticleLikeDislikeService articleLikeDislikeService, IArticleCommentReplyService articleCommentReplyService)
+        public ArticleController(IArticleService articleService, ILoginService loginservice, IArticleCommentService articleCommentService, IArticleLikeDislikeService articleLikeDislikeService, IArticleCommentReplyService articleCommentReplyService)
         {
             this.articleService = articleService;
+            this.loginService = loginservice;
             this.articleCommentService = articleCommentService;
             this.articleLikeDislikeService = articleLikeDislikeService;
             this.articleCommentReplyService = articleCommentReplyService;
@@ -31,9 +40,39 @@ namespace Api.Controllers
             var article = articleService.GetArticleById(id);
 
             if (article == null)
-                return Request.CreateResponse(HttpStatusCode.BadRequest, "didn't find article");
-            //will add image controller
-            return Request.CreateResponse(HttpStatusCode.Created, "found");
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "didn't find any article");
+
+            return Request.CreateResponse(HttpStatusCode.Created, Mapper.Map<ArticleViewModel>(article));
+        }
+        [HttpGet, JwtAuthentication(RoleEnum.Admin)]
+        public HttpResponseMessage Get()
+        {
+            var articles = articleService.GetAllArticles();
+
+            if (articles == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "didn't find any article");
+
+            return Request.CreateResponse(HttpStatusCode.Created, Mapper.Map<IEnumerable<ArticleViewModel>>(articles));
+        }
+
+        [HttpPost, JwtAuthentication(RoleEnum.Admin)]
+        public HttpResponseMessage Post([FromBody] ArticlePostModel articlePostModel)
+        {
+            if (articlePostModel == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "article is null");
+            
+            string userName = ActionContext.RequestContext.Principal.Identity.Name;
+            var user = loginService.GetUser(x => x.Username == userName);
+            
+            var article = Mapper.Map<ArticlePostModel, ArticleEntity>(articlePostModel);
+            article.ArticleOwner = user;
+
+            object status = articleService.AddNewArticle(ref article);
+
+            if (status is true)
+                return Request.CreateResponse(HttpStatusCode.Created, Mapper.Map<ArticleViewModel>(article));
+
+            return Request.CreateResponse(HttpStatusCode.BadRequest, status.ToString());
         }
     }
 }
