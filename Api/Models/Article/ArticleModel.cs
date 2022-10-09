25836a6a21152868f3ada72f6a371ffc3f0a802c
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-09 18:58:50 +0300

    first commit

---
+++ Api/Models/Article/ArticleModel.cs
@@ -0,0 +1,28 @@
+﻿using DAS.Model.Model.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models.Article
+{
+    public class ArticleViewModel
+    {
+        public Guid Id { get; set; }
+        public string ArticleTitle { get; set; }
+        public string Summary { get; set; }
+        public string Content { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public Guid ArticleOwnerId { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerSurname { get; set; }
+    }
+
+    public class ArticlePostModel
+    {
+        public string ArticleTitle { get; set; }
+        public string Summary { get; set; }
+        public string Content { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
---
+++ DAS.Model/Dto/Article/ArticleDto.cs
@@ -0,0 +1,15 @@
+﻿using DAS.Model.Model.Article;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAS.Model.Dto.Article
+{
+    [NotMapped]
+    public class ArticleDtoView : ArticleEntity
+    {
+        public Guid OwnerId { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerSurname { get; set; }
+
+    }
+}
