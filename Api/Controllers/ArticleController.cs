7094d59e792a88532ed06795ca526824885a829e
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-22 12:01:48 +0300

    added new functions and refactored

--- Api/Controllers/ArticleController.cs
+++ Api/Controllers/ArticleController.cs
@@ -44,15 +44,57 @@ namespace Api.Controllers
 
             return Request.CreateResponse(HttpStatusCode.Created, Mapper.Map<ArticleViewModel>(article));
         }
-        [HttpGet, JwtAuthentication(RoleEnum.Admin)]
-        public HttpResponseMessage Get()
+
+        [Route("getUserArticles/{id}"), JwtAuthentication(RoleEnum.Admin), CacheControl]
+        public HttpResponseMessage GetUserArticles([FromUri] Guid id)
         {
-            var articles = articleService.GetAllArticles();
+            var articles = articleService.GetUserArticles(id);
 
             if (articles == null)
                 return Request.CreateResponse(HttpStatusCode.BadRequest, "didn't find any article");
 
             return Request.CreateResponse(HttpStatusCode.Created, Mapper.Map<IEnumerable<ArticleViewModel>>(articles));
+
+        }
+
+        [Route("likeOrDislikeArticle"), JwtAuthentication(RoleEnum.Admin),HttpPost]
+        public HttpResponseMessage LikeArticle([FromBody] ArticleLikeDislikePostModel articleLikeDislikePostModel)
+        {
+            if (articleLikeDislikePostModel == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "ArticleLikeDislikePostModel is null");
+
+            string userName = ActionContext.RequestContext.Principal.Identity.Name;
+            var owner = loginService.GetUser(x => x.Username == userName);
+            var article = articleService.GetArticleEntityById(articleLikeDislikePostModel.ArticleId);
+
+            var articleLikeDislikeEntity = Mapper.Map<ArticleLikeDislikeEntity>(articleLikeDislikePostModel);
+
+            if(owner==null || article==null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "owner or/and article is/are null");
+
+
+            articleLikeDislikeEntity.ArticleLikeDislikeOwner = owner;
+            articleLikeDislikeEntity.Article = article;
+
+            var result = articleLikeDislikeService.LikeDislikeArticle(articleLikeDislikeEntity);
+
+            if (result is false)
+                return Request.CreateResponse(HttpStatusCode.BadRequest,"There is an Error");
+
+            return Request.CreateResponse(HttpStatusCode.Created, result);
+        }
+
+        [Route("IsILikeIt/{id}"),JwtAuthentication(RoleEnum.Admin),HttpGet]
+        public HttpResponseMessage IsILikeIt([FromUri] Guid id)
+        {
+            string userName = ActionContext.RequestContext.Principal.Identity.Name;
+            Guid userId = loginService.GetUserId(userName).Value;
+
+            var status = articleLikeDislikeService.GetMyStatus(id, userId);
+
+            if (status == null)
+                return Request.CreateResponse(HttpStatusCode.Accepted,"didn't find any like or dislike");
+            return Request.CreateResponse(HttpStatusCode.Created, Mapper.Map<ArticleLikeDislikeSingleViewModel>(status));
         }
 
         [HttpPost, JwtAuthentication(RoleEnum.Admin)]
@@ -60,10 +102,10 @@ namespace Api.Controllers
         {
             if (articlePostModel == null)
                 return Request.CreateResponse(HttpStatusCode.BadRequest, "article is null");
-            
+
             string userName = ActionContext.RequestContext.Principal.Identity.Name;
             var user = loginService.GetUser(x => x.Username == userName);
-            
+
             var article = Mapper.Map<ArticlePostModel, ArticleEntity>(articlePostModel);
             article.ArticleOwner = user;
 
