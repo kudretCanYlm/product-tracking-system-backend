f7db0a465595eb9685fc46f7a86af4f3122597cb
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-22 12:02:04 +0300

    first commit

---
+++ DAS.Model/Dto/Article/ArticleLikeDislikeEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Model.Dto.Article
+{
+    [NotMapped]
+    public class ArticleLikeDtoViewSingle
+    {
+        public bool İsLiked { get; set; }
+    }
+}
