e6a7483aee098bcece9684107b0815feeb0a6523
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-22 11:59:32 +0300

    added new functions

--- DAS.Service/Services/Article/ArticleLikeDislikeService.cs
+++ DAS.Service/Services/Article/ArticleLikeDislikeService.cs
@@ -1,6 +1,8 @@
 ﻿using DAS.Core.Infrastructure;
 using DAS.Core.Repository.Article;
+using DAS.Model.Dto.Article;
 using DAS.Model.Model.Article;
+using DAS.Service.common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,10 @@ namespace DAS.Service.Services.Article
 {
     public interface IArticleLikeDislikeService
     {
-
+        object LikeDislikeArticle(ArticleLikeDislikeEntity articleLikeDislikeEntity);
+        ArticleLikeDislikeEntity GetMyStatus(Guid articleId,Guid userId);
     }
-    public class ArticleLikeDislikeService:IArticleLikeDislikeService
+    public class ArticleLikeDislikeService : IArticleLikeDislikeService
     {
         private readonly IArticleLikeDislikeRepository articleLikeDislikeRepository;
         private readonly IUnitOfWork unitOfWork;
@@ -25,5 +28,111 @@ namespace DAS.Service.Services.Article
             this.unitOfWork = unitOfWork;
             this.validator = validator;
         }
+
+        public object LikeDislikeArticle(ArticleLikeDislikeEntity articleLikeDislikeEntity)
+        {
+            articleLikeDislikeEntity.CreatedAt = DateTime.Now;
+
+            if (validator.IsValidEntity(articleLikeDislikeEntity))
+            {
+                string message;
+
+                if (articleLikeDislikeEntity.Isliked)
+                {
+                    
+                    //like sets
+                    if(articleLikeDislikeRepository.isArticleLikedOrDisliked(articleLikeDislikeEntity))
+                    {
+                        //delete myliked row
+                        articleLikeDislikeRepository.DeleteLikeOrDislike(articleLikeDislikeEntity);
+                        message = "liked is removed";
+                    }
+                    else
+                    {
+                        //remove mydisliked row
+                        articleLikeDislikeRepository.DeleteOpposite(articleLikeDislikeEntity);
+
+                        //add meliked row
+                        articleLikeDislikeRepository.Add(articleLikeDislikeEntity);
+                        message = "liked is added";
+                    }
+                }
+                else
+                {
+                    //dislike sets
+                    if(articleLikeDislikeRepository.isArticleLikedOrDisliked(articleLikeDislikeEntity))
+                    {
+                        //remove mydisliked row
+                        articleLikeDislikeRepository.DeleteLikeOrDislike(articleLikeDislikeEntity);
+                        message = "disliked is removed";
+                    }
+                    else
+                    {
+                        //remove liked row
+                        articleLikeDislikeRepository.DeleteOpposite(articleLikeDislikeEntity);
+                        //add mydisliked row
+                        articleLikeDislikeRepository.Add(articleLikeDislikeEntity);
+                        message =  "disliked is added" ;
+                    }
+                }
+
+                try
+                {
+                    SaveArticleLikeDislike();
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+
+
+                return message;
+            }
+
+            return validator.GetValidErrorMessages(articleLikeDislikeEntity);
+        }
+
+        public ArticleLikeDislikeEntity GetMyStatus(Guid articleId, Guid userId)
+        {
+            return articleLikeDislikeRepository.Get(x => x.DeletedAt == null && x.ArticleId==articleId && x.ArticleLikeDislikeOwnerId==userId) ?? null;
+        }
+
+        private object SaveArticleLikeDislikeWithMessage()
+        {
+            try
+            {
+                unitOfWork.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+        }
+
+        private void SaveArticleLikeDislike()
+        {
+            unitOfWork.Commit();
+        }
+
+        private dynamic TransactionArticleLikeDislike()
+        {
+            try
+            {
+                unitOfWork.BeginTransaction();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                unitOfWork.RollBack();
+
+                return ex.Message;
+            }
+        }
+
+        
     }
 }
