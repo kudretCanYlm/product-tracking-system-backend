9a38b7a5d6e447fd6a97a4af6784942514208e1f
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-22 11:58:31 +0300

    first commit

---
+++ Api/Models/Article/ArticleLikeDislikeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models.Article
+{
+    public class ArticleLikeDislikePostModel
+    {
+        public bool Isliked { get; set; }   
+        public Guid ArticleId { get; set; }
+    }
+
+    public class ArticleLikeDislikeSingleViewModel
+    {
+        public bool Isliked { get; set; }
+    }
+}
