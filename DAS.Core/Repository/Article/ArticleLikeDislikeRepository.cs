c4934c9a26b0cca50232d0877686e19bf591855b
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-22 12:00:42 +0300

    added new functions

--- DAS.Core/Repository/Article/ArticleLikeDislikeRepository.cs
+++ DAS.Core/Repository/Article/ArticleLikeDislikeRepository.cs
@@ -8,16 +8,71 @@ using System.Threading.Tasks;
 
 namespace DAS.Core.Repository.Article
 {
-    public class ArticleLikeDislikeRepository:RepositoryBase<ArticleLikeDislikeEntity>, IArticleLikeDislikeRepository
+    public class ArticleLikeDislikeRepository : RepositoryBase<ArticleLikeDislikeEntity>, IArticleLikeDislikeRepository
     {
-        public ArticleLikeDislikeRepository(IDatabaseFactory databaseFactory):base(databaseFactory)
+        public ArticleLikeDislikeRepository(IDatabaseFactory databaseFactory) : base(databaseFactory)
         {
 
         }
+
+        public bool isArticleLikedOrDisliked(ArticleLikeDislikeEntity articleLikeDislikeEntity)
+        {
+            return GetManyQuery(
+                    x => x.ArticleId == articleLikeDislikeEntity.Article.Id
+                    &&
+                    x.ArticleLikeDislikeOwnerId == articleLikeDislikeEntity.ArticleLikeDislikeOwner.Id
+                    &&
+                    x.Isliked == articleLikeDislikeEntity.Isliked
+                    &&
+                    x.DeletedAt.Value.ToString().Length <= 4
+                   ).Count() > 0;
+        }
+
+        public void DeleteLikeOrDislike(ArticleLikeDislikeEntity articleLikeDislikeEntity)
+        {
+            var entityDelete = Get(
+                    x => x.ArticleId == articleLikeDislikeEntity.Article.Id
+                    &&
+                    x.ArticleLikeDislikeOwnerId == articleLikeDislikeEntity.ArticleLikeDislikeOwner.Id
+                    &&
+                    x.Isliked == articleLikeDislikeEntity.Isliked
+                    &&
+                    x.DeletedAt == null
+                );
+
+            if (entityDelete != null)
+            {
+                entityDelete.DeletedAt = DateTime.Now;
+
+                Update(entityDelete);
+            }
+        }
+
+        public void DeleteOpposite(ArticleLikeDislikeEntity articleLikeDislikeEntity)
+        {
+            var entityDelete = Get(
+                    x => x.ArticleId == articleLikeDislikeEntity.Article.Id
+                    &&
+                    x.ArticleLikeDislikeOwnerId == articleLikeDislikeEntity.ArticleLikeDislikeOwner.Id
+                    &&
+                    x.Isliked == !articleLikeDislikeEntity.Isliked
+                    &&
+                    x.DeletedAt == null
+            );
+
+            if (entityDelete != null)
+            {
+                entityDelete.DeletedAt = DateTime.Now;
+
+                Update(entityDelete);
+            }
+        }
     }
 
-    public interface IArticleLikeDislikeRepository:IRepository<ArticleLikeDislikeEntity>
+    public interface IArticleLikeDislikeRepository : IRepository<ArticleLikeDislikeEntity>
     {
-
+        bool isArticleLikedOrDisliked(ArticleLikeDislikeEntity articleLikeDislikeEntity);
+        void DeleteLikeOrDislike(ArticleLikeDislikeEntity articleLikeDislikeEntity);
+        void DeleteOpposite(ArticleLikeDislikeEntity articleLikeDislikeEntity);
     }
 }
