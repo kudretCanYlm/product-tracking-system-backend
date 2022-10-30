57c19d41adf6c82dd8cf7ff7021a0fb075af744c
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-30 11:51:31 +0300

    first commit

---
+++ Api/Models/Article/ArticleCommentModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models.Article
+{
+    public class ArticleCommentViewModel
+    {
+        public Guid Id { get; set; }
+        public string Comment { get; set; }
+        public Guid CommentOwnerId { get; set; }
+        public string CommentOwnerName { get; set; }
+        public string CommentOwnerSurname { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+    }
+
+    public class ArticleCommentPostModel
+    {
+        public string Comment { get; set; }
+        public Guid ArticleId { get; set; }
+    }
+
+}
---
+++ DAS.Model/Dto/Article/ArticleCommentDto.cs
@@ -0,0 +1,18 @@
+﻿using DAS.Model.Model.Article;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Model.Dto.Article
+{
+    [NotMapped]
+    public class ArticleCommentDto:ArticleCommentEntity
+    {
+
+        public string CommentOwnerName { get; set; }
+        public string CommentOwnerSurname { get; set; }
+    }
+}
