faabb8ece0127e2f903d9af3be5317488cd28f89
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-10-30 11:52:35 +0300

    added new methods and an class

--- DAS.Core/Repository/Article/ArticleCommentRepository.cs
+++ DAS.Core/Repository/Article/ArticleCommentRepository.cs
@@ -1,8 +1,10 @@
 ﻿using DAS.Core.Infrastructure;
+using DAS.Model.Dto.Article;
 using DAS.Model.Model.Article;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,10 +15,28 @@ namespace DAS.Core.Repository.Article
         public ArticleCommentRepository(IDatabaseFactory databaseFactory) : base(databaseFactory)
         {
         }
+
+        public IEnumerable<ArticleCommentDto> GetDtos(Guid articleId)
+        {
+            return GetManyQuery(x => x.ArticleId == articleId && x.DeletedAt==null).Select(ArticleCommentRepositorySelects.articleCommmentViewDto).ToList();
+        }
     }
 
     public interface IArticleCommentRepository : IRepository<ArticleCommentEntity>
     {
+        IEnumerable<ArticleCommentDto> GetDtos(Guid articleId);
+    }
 
+    public  class ArticleCommentRepositorySelects
+    {
+        public static Expression<Func<ArticleCommentEntity, ArticleCommentDto>> articleCommmentViewDto = articleComment => new ArticleCommentDto { 
+            
+            Id=articleComment.Id,
+            Comment=articleComment.Comment,
+            CommentOwnerId=articleComment.CommentOwnerId,
+            CommentOwnerName=articleComment.CommentOwner.Name,
+            CommentOwnerSurname=articleComment.CommentOwner.Surname,
+            CreatedAt=articleComment.CreatedAt
+        };
     }
 }
