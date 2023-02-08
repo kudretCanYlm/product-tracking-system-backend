32b23598e848c5ac32317b8d23912ef09ffa8533
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-02-08 12:48:59 +0300

    added new props

--- DAS.Model/Model/Company/WorkerRoleEntity.cs
+++ DAS.Model/Model/Company/WorkerRoleEntity.cs
@@ -1,8 +1,10 @@
 ﻿using DAS.Model.Base;
 using DAS.Model.Base.Interfaces;
+using DAS.Model.Model.Authentication;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +27,14 @@ namespace DAS.Model.Model.Company
         public DateTime? CreatedAt { get; set; }
         public DateTime? ModifiedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
+        public Guid WorkerId { get; set; }
+        public Guid CompanyId { get; set; }
+
+        [ForeignKey(nameof(WorkerId))]
+        public LoginEntity Worker { get; set; }
+        
+        [ForeignKey(nameof(CompanyId))]
+        public CompanyEntity Company { get; set; }
     }
 
     public interface IWorkerRoleValidation : IValidator<WorkerRoleEntity>
