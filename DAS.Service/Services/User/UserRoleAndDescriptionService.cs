9322e885ad56fbf9d53157978a79d1e1164c8b8e
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-03-01 16:19:03 +0300

    added new services

--- DAS.Service/Services/User/UserRoleAndDescriptionService.cs
+++ DAS.Service/Services/User/UserRoleAndDescriptionService.cs
@@ -1,10 +1,14 @@
 ﻿using DAS.Core.Infrastructure;
 using DAS.Core.Repository.Authentication;
+using DAS.Core.Repository.Project;
 using DAS.Core.Repository.User;
+using DAS.Model.Model.Project;
 using DAS.Model.Model.User;
+using DAS.Service.common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,12 +17,14 @@ namespace DAS.Service.Services.User
 	public interface IUserRoleAndDescriptionService
 	{
 		object AddUserRoleAndDescription(UserRoleAndDescriptionEntity entity);
+		IEnumerable<UserRoleAndDescriptionEntity> GetUserRoleAndDescriptionByRange<TOrder>(int pageNumber, int pageSize, Expression<Func<UserRoleAndDescriptionEntity, TOrder>> orderBy);
 	}
 	public class UserRoleAndDescriptionService:IUserRoleAndDescriptionService
 	{
 		private readonly ILoginRepository loginRepository;
 		private readonly IUserRoleAndDescriptionRepository userRoleAndDescriptionRepository;
 		private readonly IUnitOfWork unitOfWork;
+		private readonly IUserRoleAndDescriptionValidation validator;
 
 		public UserRoleAndDescriptionService(ILoginRepository loginRepository, IUserRoleAndDescriptionRepository userRoleAndDescriptionRepository, IUnitOfWork unitOfWork)
 		{
@@ -29,7 +35,38 @@ namespace DAS.Service.Services.User
 
 		public object AddUserRoleAndDescription(UserRoleAndDescriptionEntity entity)
 		{
-			throw new NotImplementedException();
+			entity.CreatedAt = DateTime.Now;
+			if(validator.IsValidEntity(entity))
+			{
+				userRoleAndDescriptionRepository.Add(entity);
+				return SaveProjectWithMessage();
+			}
+
+			return validator.GetValidErrorMessages(entity);
+		}
+
+		public IEnumerable<UserRoleAndDescriptionEntity> GetUserRoleAndDescriptionByRange<TOrder>(int pageNumber, int pageSize, Expression<Func<UserRoleAndDescriptionEntity, TOrder>> orderBy)
+		{
+			var page = new Page(pageNumber, pageSize);
+			if (page.Skip < 0)
+				return null;
+			var userRoleAndDescriptions = userRoleAndDescriptionRepository.GetPage(page, x => true, orderBy).ToList();
+
+			return userRoleAndDescriptions;
+		}
+
+		private object SaveProjectWithMessage()
+		{
+			try
+			{
+				unitOfWork.Commit();
+				return true;
+			}
+			catch (Exception ex)
+			{
+
+				return ex.Message.ToString();
+			}
 		}
 	}
 
