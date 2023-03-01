0eeb02da2238d201ea9ff911a32e815a5b43ffbf
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-03-01 16:17:59 +0300

    first commit

---
+++ DAS.Core/Specifications/ArticleWithOwnerSpecifications.cs
@@ -0,0 +1,28 @@
+﻿using DAS.Core.Specifications.Base;
+using DAS.Model.Model.Article;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Core.Specifications
+{
+	public class ArticleWithOwnerSpecifications : BaseSpecification<ArticleEntity>
+	{
+		public ArticleWithOwnerSpecifications(string articleTitle) : base(art => art.ArticleTitle.Contains(articleTitle))
+		{
+			AddInclude(art => art.ArticleOwner);
+		}
+
+		public ArticleWithOwnerSpecifications(Guid ArticleId) : base(art => art.Id == ArticleId)
+		{
+			AddInclude(art => art.ArticleOwner);
+		}
+
+		public ArticleWithOwnerSpecifications() : base(null)
+		{
+			AddInclude(art => art.ArticleOwner);
+		}
+	}
+}
