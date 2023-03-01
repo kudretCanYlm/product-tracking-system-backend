5f8fbd2c9b578763ae0f45ade2e881ad966dbc9a
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-03-01 16:16:46 +0300

    first commit

---
+++ Api/Models/User/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models.User
+{
+	public class UserModel
+	{
+		 public class UserRoleAndDescriptionViewModel
+		{
+			public Guid OwnerUserId { get; set; }
+			public string FullName { get; set; }
+			public string MiniRole { get; set; }
+			public string MiniDescription { get; set; }
+		}
+
+		public class UserRoleAndDescriptionPostModel
+		{
+			public Guid? OwnerUserId { get; set; }
+			public string MiniRole { get; set; }
+			public string MiniDescription { get; set; }
+		}
+	}
+}
