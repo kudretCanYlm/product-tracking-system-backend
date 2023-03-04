bd833c66055128294baa86dc59fda2dbc0b51d3a
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-03-04 12:46:20 +0300

    first commit

---
+++ DAS.Core/PagingAndFiltering/PagedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Core.PagingAndFiltering
+{
+	public class PagedList<T> : IPagedList<T>
+	{
+		public PagedList(int pageIndex, int pageSize, int totalCount, int totalPages, IEnumerable<T> items)
+		{
+			PageIndex = pageIndex;
+			PageSize = pageSize;
+			TotalCount = totalCount;
+			TotalPages = totalPages;
+			Items = items;
+		}
+
+		public PagedList(IQueryable<T> query, PagingArgs pagingArgs, List<(SortingOption, Expression<Func<T, object>>)> orderByList = null, List<(FilteringOption, Expression<Func<T, bool>>)> filterList = null)
+		{
+			query = query.MyOrderBy(orderByList);
+			query = query.MyWhere(filterList);
+
+			PageIndex = pagingArgs.PageIndex < 1 ? 1 : pagingArgs.PageIndex;
+			PageSize = pagingArgs.PageSize < 1 ? 1 : pagingArgs.PageSize;
+
+			TotalCount = 0;
+
+			var items =
+				pagingArgs.PagingStrategy == PagingStrategy.NoCount
+				?
+				query.Skip((PageIndex - 1) * PageSize).Take(PageSize + 1).ToList()
+				:
+				(
+					(TotalCount = query.Count()) > 0
+						?
+						query.Skip((PageIndex - 1) * PageSize)
+							.Take(PageSize).ToList()
+							: new List<T>()
+				);
+			TotalCount = pagingArgs.PagingStrategy == PagingStrategy.WithCount
+				? (PageIndex - 1) * PageSize + items.Count : TotalCount;
+
+			TotalPages = TotalCount / PageSize;
+
+			if (TotalPages % PageSize > 0)
+				TotalPages++;
+
+			if (pagingArgs.PagingStrategy == PagingStrategy.NoCount && items.Count == PageSize + 1)
+				items.RemoveAt(PageSize);
+
+			Items = items;
+		}
+		public int PageIndex { get; }
+
+		public int PageSize { get; }
+
+		public int TotalCount { get; }
+
+		public int TotalPages { get; }
+
+		public bool HasPreviousPage => PageIndex > 0;
+
+		public bool HasNextPage => PageIndex + 1 < TotalCount;
+
+		public IEnumerable<T> Items { get; }
+
+		
+	}
+}
---
+++ DAS.Core/PagingAndFiltering/PagingExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAS.Core.PagingAndFiltering
+{
+	public static class PagingExtensions
+	{
+		public static IQueryable<T> MyOrderBy<T>(this IQueryable<T> query, List<(SortingOption, Expression<Func<T, object>>)> orderByList)
+		{
+			if (orderByList == null)
+				return query;
+
+			orderByList = orderByList.OrderBy(ob => ob.Item1.Priority).ToList();
+
+			IOrderedQueryable<T> orderedQuery = null;
+
+			foreach (var orderBy in orderByList)
+			{
+				if (orderedQuery == null)
+					orderedQuery = orderBy.Item1.Direction == SortingDirection.ASC ? query.OrderBy(orderBy.Item2) : query.OrderByDescending(orderBy.Item2);
+				else
+					orderedQuery = orderBy.Item1.Direction == SortingDirection.ASC ? orderedQuery.ThenBy(orderBy.Item2) : orderedQuery.ThenByDescending(orderBy.Item2);
+			}
+
+			return orderedQuery;
+		}
+
+		public static IQueryable<T> MyWhere<T>(this IQueryable<T> query, List<(FilteringOption, Expression<Func<T, bool>>)> filterList = null)
+		{
+			if (filterList == null)
+				return query;
+
+			foreach (var item in filterList)
+			{
+				query=query.Where(item.Item2);
+			}
+
+			return query;
+		}
+	}
+}
