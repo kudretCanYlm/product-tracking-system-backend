a08ade699fe49d726bd62e7f3ef7b38c8271e3f8
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-21 15:09:26 +0300

    First commit

---
+++ Api/Models/Login/AuthorizedUserInformationMiniModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models.Login
+{
+	public class AuthorizedUserInformationMiniModel
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Email { get; set; }
+	}
+}
